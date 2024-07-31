eed32be6e1ef0f0d44fdbd702ed3c4298bfc9a32
helen-cho <hd9536@daum.net>
2024-07-31 11:15:48 +0900

    웹 Laoyout

--- web/ex01/app.py
+++ web/ex01/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
 def index():
-  return "hello.........."
+  return render_template("index.html", title="홈페이지", pageName='home.html')
 
 
 if __name__ == '__main__':
