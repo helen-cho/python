fd3610ecb4ad21aec23b8e712e82799df2f4d197
helen-cho <hd9536@daum.net>
2025-09-18 17:42:59 +0900

    09-18

--- 04.데이터시각화/웹페이지/web04/app.py
+++ 04.데이터시각화/웹페이지/web04/app.py
@@ -11,10 +11,18 @@ def score():
 def score_data():
     word = request.args['word']
     df = pd.read_csv(f'{app.root_path}/static/score.csv')
+
+    df['평균'] = df.apply(lambda row:row['국어':'사회'].mean(), axis=1)
     filt = (df['이름'].str.contains(word)) | (df['학교'].str.contains(word))
-    df2 = df[filt]
-    table = df2.to_html(classes='table table-striped table-hover', index=False)
-    return table
+    df = df[filt]
+
+    df_info = df[['지원번호', '이름', '학교', '키', 'SW특기']]
+    df_score = df[['지원번호', '이름', '국어','영어','수학', '과학', '사회', '평균']]
+
+    table_info = df_info.to_html(classes='table table-striped table-hover', index=False)
+    table_score = df_score.to_html(classes='table table-striped table-hover', index=False)
+    data = {'info':table_info, 'score':table_score}
+    return data
 
 if __name__=='__main__':
     app.run(port=5000, debug=True)
