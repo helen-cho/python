3bf4204430b35fa0bfea6107dabffdab0157ba92
helen-cho <hd9536@daum.net>
2025-09-19 17:43:00 +0900

    09-19

--- 04.데이터시각화/웹페이지/web05/app.py
+++ 04.데이터시각화/웹페이지/web05/app.py
@@ -11,7 +11,12 @@ def health_data():
     import pandas as pd
     page = int(request.args['page'])
     size = int(request.args['size'])
+    word = request.args['word']
+
     df = pd.read_csv(f'{app.root_path}/data/인구수별공공의료기관수.csv')
+    filt = df['시도군구'].str.contains(word)
+    df = df[filt]
+    
     start = (page-1) * size
     end = page * size
     df2 = df[start:end]
