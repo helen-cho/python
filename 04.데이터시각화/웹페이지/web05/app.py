9aaa233044c578f39311c70c7ec4355ebd5b606d
helen-cho <hd9536@daum.net>
2025-09-19 16:58:59 +0900

    09-19

--- 04.데이터시각화/웹페이지/web05/app.py
+++ 04.데이터시각화/웹페이지/web05/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='temp')
 
@@ -9,8 +9,12 @@ def health():
 @app.route('/health/data')
 def health_data():
     import pandas as pd
+    page = int(request.args['page'])
+    size = int(request.args['size'])
     df = pd.read_csv(f'{app.root_path}/data/인구수별공공의료기관수.csv')
-    df2 = df[:10]
+    start = (page-1) * size
+    end = page * size
+    df2 = df[start:end]
     count = len(df)
     table = df2.to_html(index=True, classes='table table-striped table-hover')
 
