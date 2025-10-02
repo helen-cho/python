747b5e0d5d20469c1ddf3c96487317f65792084f
helen-cho <hd9536@daum.net>
2025-10-02 16:15:18 +0900

    10-02

--- 05.인공지능/웹페이지/web09/app.py
+++ 05.인공지능/웹페이지/web09/app.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, request
 import pandas as pd
-
+from naverapi import getNew 
 
 app = Flask(__name__, template_folder='temp', static_folder='static')
 
+@app.route('/search')
+def search():
+    start=1
+    display=5
+    query = request.args['query']
+    result = getNew(query, start, display)
+    return result
 
 @app.route('/')
 def index():
