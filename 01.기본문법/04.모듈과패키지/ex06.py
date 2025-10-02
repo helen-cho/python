25488957f49f3a581cbaf4d391a3a27c0acd7a92
helen-cho <hd9536@daum.net>
2025-10-02 12:17:57 +0900

    10-20

---
+++ 01.기본문법/04.모듈과패키지/ex06.py
@@ -0,0 +1,7 @@
+#import travel.thailand.trip1 오류발생
+
+import travel.thailand
+import travel.vietnam as vtravel
+
+travel.thailand.trip1()
+vtravel.trip1()
---
+++ 01.기본문법/04.모듈과패키지/ex07.py
@@ -0,0 +1,7 @@
+from travel import thailand, vietnam
+
+thailand.trip1()
+thailand.trip2()
+
+vietnam.trip1()
+vietnam.trip2()
