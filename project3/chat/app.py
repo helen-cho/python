7c3deebc456ac4ac609b244b9384083db3752618
helen-cho <hd9536@daum.net>
2025-10-14 11:42:27 +0900

    10-14

--- project3/chat/app.py
+++ project3/chat/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Flask, render_template, request, session
+from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__, template_folder='temp')
 app.secret_key='1234'
@@ -7,7 +7,25 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='채팅방', pageName='home.html')
+    return render_template('index.html', title='Home', pageName='home.html')
+
+@app.route('/chat/<room>')
+def chat(room):
+    uid = request.args['uid']
+    session['uid'] = uid
+    session['room'] = room
+    room_names  = ['친구', '가족', '회사']
+    room_name = room_names[int(room)]
+    return render_template('index.html', title='채팅방', pageName='chat.html', 
+                           room=room_name, uid=uid)
+
+@socketio.on('joined', namespace='/chat')
+def joined():
+    uid = session['uid']
+    room = session['room']
+    join_room(room)
+    msg = f'{uid}님 입장하셨습니다.'
+    emit('status', {'msg':msg}, room=room)
 
 if __name__=='__main__':
     socketio.run(app, port=5000, debug=True)
